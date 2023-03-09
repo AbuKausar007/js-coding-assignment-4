c264991c0562cc2691929212bc6c2eb9ed379862
AbuKausar007 <a.25kausar@gmail.com>
2023-03-09 16:31:16 +0600

    all problem done

--- problemsolve.js
+++ problemsolve.js
@@ -11,7 +11,7 @@ function mindGame(number) {
         return subtract;
 }
 
-const inputGiven = false;
+const inputGiven = 5;
 const result = mindGame(inputGiven);
 console.log(result);
 
@@ -35,54 +35,63 @@ console.log(stringOutput);
 
 // Problem 3: A function called isLGSeven, it takes number as input. You have to differentiate between input and 7. If the difference is less than 7, return the subtraction otherwise return double of the input value.
 
-// function isLGSeven(num) {
-//     if (num - 7 < 7){
-//         return (num - 7);
-//     }
-//     else if(num - 7 >= 7){
-//         return num*2;
-//     }
-// }
+function isLGSeven(num) {
+    if (typeof num !== 'number') {
+        return "Please enter only number!!";
+    }  
+    if (num - 7 < 7){
+        return (num - 7);
+    }
+    else if(num - 7 >= 7){
+        return num*2;
+    }
+}
 
-// const numInput = 15;
-// const numOutputIs = isLGSeven(numInput);
-// console.log(numOutputIs)
+const numInput = 6;
+const numOutputIs = isLGSeven(numInput);
+console.log(numOutputIs)
 
 
 // Problem 4: A function named findingBadData, which takes array as input. Array may contain positive or negative numbers. If function finds negative numbers then it returns how many negative numbers is there.
 
-// function findingBadData(array) {
-//     let badData = [];
-//     for(let i = 0; i <= array.length; i++) {
-//         const element = array[i];
-//         if(element < 0){
-//             badData.push(element);
+function findingBadData(arr) {
+    let badData = [];
+    if (!Array.isArray(arr)) {
+        return "Please enter array!!";
+    }
+    for(let i = 0; i <= arr.length; i++) {
+        const element = arr[i];
+        if(element < 0){
+            badData.push(element);
             
-//         }
+        }
         
-//     }
-//     return badData.length;   
-// }
-// const inputArray = [1,2,5];
-// const outputArray = findingBadData(inputArray);
-// console.log(outputArray);
+    }
+    return badData.length;   
+}
+const inputArray = [1,2,3];
+const outputArray = findingBadData(inputArray);
+console.log(outputArray);
 
 
 // Problem 5: A function named gemsToDiamond, which takes 3 input parameters. 1st parameter gems power is 21, 2nd gems power 32, 3rd gems power 43. 
 
-// function gemsToDiamond(num1,num2,num3){
-//     const   gemNum1Produce = num1 * 21;
-//     const   gemNum2Produce = num2 * 32;
-//     const   gemNum3Produce = num3 * 43;
-//     const   diamondProduce = gemNum1Produce + gemNum2Produce + gemNum3Produce;
-//     if (diamondProduce >= 1000*2){
-//         const diamondWeWillGet = diamondProduce - 2000;
-//         return diamondWeWillGet;
-//     }
-//     else if(diamondProduce < 1000*2){
-//         return diamondProduce;
-//     }
-// }
+function gemsToDiamond(num1,num2,num3){
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' || typeof num3 !== 'number') {
+        return "Please give number only!!";
+    }
+    const   gemNum1Produce = num1 * 21;
+    const   gemNum2Produce = num2 * 32;
+    const   gemNum3Produce = num3 * 43;
+    const   diamondProduce = gemNum1Produce + gemNum2Produce + gemNum3Produce;
+    if (diamondProduce >= 1000*2){
+        const diamondWeWillGet = diamondProduce - 2000;
+        return diamondWeWillGet;
+    }
+    else if(diamondProduce < 1000*2){
+        return diamondProduce;
+    }
+}
 
-// const totalDiamond = gemsToDiamond(100,5,1);
-// console.log(totalDiamond);
+const totalDiamond = gemsToDiamond(100,5,1);
+console.log(totalDiamond);
